44e4a697fd959531d53670988dace1d102c7ce46
jsj9871 <75598682+jsj9871@users.noreply.github.com>
2022-10-19 21:50:28 +0900

    2022.10.19(참조자)

---
+++ 2022.10.18(클래스 구현부, 선언부).h
@@ -0,0 +1,47 @@
+#include <iostream>	// 입출력 스트림
+#include "Monster.h"
+
+using namespace std;
+
+int main()
+{
+	// 클래스 구현부
+	/*
+	Monster monster1;
+	Monster monster2;
+
+	monster1.Attack(10);
+	*/
+
+	// 영수증
+	/*
+	int sum = 0;
+	int temp = 0;
+	int thing = 0;
+
+	int price = 0;
+	int thingNumber = 0;
+
+	cin >> sum;
+	cin >> thing;
+
+	for (int i = 0; i < thing; i++)
+	{
+		cin >> price;
+		cin >> thingNumber;
+
+		temp += price * thingNumber;
+	}
+
+	if (sum == temp)
+	{
+		cout << "Yes";
+	}
+	else
+	{
+		cout << "No";
+	}
+	*/
+
+	return 0;
+}
